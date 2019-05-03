8cc7338ef5eba549f10026296f1534253a408584
Sergio Andrade <sergio.david2201@gmail.com>
2019-05-03 16:17:17 -0300

    Primera parte del encerrado

--- pengines-master/apps/proylcc/proylcc.pl
+++ pengines-master/apps/proylcc/proylcc.pl
@@ -35,7 +35,8 @@ emptyBoard([
 
 goMove(Board, Player, [R,C], RBoard):-
     replace(Row, R, NRow, Board, RBoard),
-    replace("-", C, Player, Row, NRow).
+    replace("-", C, Player, Row, NRow),
+    \+(encerrado(RBoard, Player, R, C, [], X)).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -50,28 +51,28 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
     XIndexS is XIndex - 1,
     replace(X, XIndexS, Y, Xs, XsY).
 
-/**
-encerrado(Board, Player, [R,C]).
-	adyacetes(Board, Player, [R,C]), ListaAdyaventes),
-*/
+%Caso base, funca 
+encerrado(Board, Player, R, C, Encerradas, [[Player, R, C]|Encerradas]):-
+	adyacentes(Board, R, C, [FichaArriba, IndexArriba, Columna], [FichaAbajo, IndexAbajo, Columna], [FichaDerecha, Fila, IndexDerecha], [FichaIzquierda, Fila, IndexIzquierda]),
+	((FichaArriba \= Player, FichaArriba \= "-") ; member([FichaArriba, IndexArriba, Columna], Encerradas)),
+	((FichaAbajo \= Player, FichaAbajo \= "-") ; member([FichaAbajo, IndexAbajo, Columna], Encerradas)),
+	((FichaDerecha \= Player, FichaDerecha \= "-") ; member([FichaDerecha, Fila, IndexDerecha], Encerradas)),
+	((FichaIzquierda \= Player, FichaIzquierda \= "-") ; member([FichaIzquierda, Fila, IndexIzquierda], Encerradas)).
 
-
-adyacentes(Board, [Player, [Fila, Columna]], [FichaArriba, FichaAbajo, FichaDerecha, FichaIzquierda]):-
+adyacentes(Board, Fila, Columna, [FichaArriba, IndexArriba, Columna], [FichaAbajo, IndexAbajo, Columna], [FichaDerecha, Fila, IndexDerecha], [FichaIzquierda, Fila, IndexIzquierda]):-
 	IndexArriba is Fila - 1, IndexAbajo is Fila + 1, IndexDerecha is Columna + 1, IndexIzquierda is Columna - 1,
-
+	%Obtiene la ficha adyacente arriba
 	getElem(FilaArriba, IndexArriba, Board),
 	getElem(FichaArriba, Columna, FilaArriba),
-
+	%Obtiene la ficha adyacente abajo
 	getElem(FilaAbajo, IndexAbajo, Board),
 	getElem(FichaAbajo, Columna, FilaAbajo),
-
+	%%Obtiene la fila actual
 	getElem(FilaActual, Fila, Board),
-
+	%Obtiene la ficha adyacente derecha
 	getElem(FichaDerecha, IndexDerecha, FilaActual),
-
-	getElem(FichaIzquierda, IndexIzquierda, FilaActual)
-
-	.
+	%Obtiene la ficha adyacente izquierda
+	getElem(FichaIzquierda, IndexIzquierda, FilaActual).
 
 getElem(X, 0, [X|Xs]).
 
