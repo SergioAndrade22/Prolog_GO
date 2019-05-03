680c5ec8b464fbb4cda0b45a6a0ac4936fc68a8d
tomasZarate <e.tomas.zarate@gmail.com>
2019-05-03 19:56:33 -0300

    Algoritmo falopa no testeado
    Momento de inspiración, encerrados, no probé nada, puede fallar

--- pengines-master/apps/proylcc/proylcc.pl
+++ pengines-master/apps/proylcc/proylcc.pl
@@ -59,6 +59,26 @@ encerrado(Board, Player, R, C, Encerradas, [[Player, R, C]|Encerradas]):-
 	((FichaDerecha \= Player, FichaDerecha \= "-") ; member([FichaDerecha, Fila, IndexDerecha], Encerradas)),
 	((FichaIzquierda \= Player, FichaIzquierda \= "-") ; member([FichaIzquierda, Fila, IndexIzquierda], Encerradas)).
 
+
+%%Encerrados Tomi
+%Caso Base, cuando no quedan mas adyacentes por revisar, asumo que estoy encerrado
+encerrados(Board,Player,R,C,[],Vistos,[[Player,R,C]]).
+
+%Caso recursivo, donde el siguiente adyacente es del mismo color, chequeo si esta encerrado y sus adyacentes, y reviso el resto de mis adyacentes
+encerrados(Board,Player,R,C,[[Player, RAd, CAd]|Adyacentes],Vistos,[[Player,R,C]|Encerrados]):-
+	\+(member([Player, RAd, CAd],Vistos)),
+	adyacentes(Board,R,C,RAdyacentes),
+	encerrados(Board,Player,RAd,CAd,RAdyacentes,[[Player,R,C]|Vistos],EncerradosAd,),%No se que tan legal es hacer meter [[Player,R,C]|Vistos]
+	encerrados(Board,Player,R,C,Adyacentes,[[Player,R,C]|Vistos],EncerradosP),
+	append(EncerradosP,EncerradosAd,Encerrados).
+	
+%Caso recursivo, donde el siguiente adyacente es del color opuesto, chequeo los demas adyacentes
+encerrados(Board,Player,R,C,[[Player2, RAd, CAd]|Adyacentes],Vistos,Encerrados):-
+	Player2 \= Player,
+	Player2 \= "-",
+	encerrados(Board,Player,R,C,Adyacentes,Vistos,Encerrados).
+	
+
 adyacentes(Board, Fila, Columna, [FichaArriba, IndexArriba, Columna], [FichaAbajo, IndexAbajo, Columna], [FichaDerecha, Fila, IndexDerecha], [FichaIzquierda, Fila, IndexIzquierda]):-
 	IndexArriba is Fila - 1, IndexAbajo is Fila + 1, IndexDerecha is Columna + 1, IndexIzquierda is Columna - 1,
 	%Obtiene la ficha adyacente arriba
